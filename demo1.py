e3bb5b990afe0e1a5b2eee37e74bb13ea7ccb36c
RikardMiklec <rikardmiklec1@gmail.com>
2023-11-21 19:46:34 +0000

    c

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print("Dobro dosli u pytdev program")
+
+first_name = "Pero"
+last_name = "Peric"
+birth = 1998
+mobile_phone = "+385 9n 1523 311"
+employed = True
+height = 180.99
+
+print(first_name)
